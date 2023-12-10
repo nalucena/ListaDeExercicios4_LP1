644e5ce270b1e9c94102afb9d5a6037d14bcb549
Nathalya Lucena Vieira de Melo <nlucena.dev@gmail.com>
2023-12-10 18:10:48 -0300

    Atualização da questão

--- src/Quest5.java
+++ src/Quest5.java
@@ -1,16 +1,27 @@
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Quest5 {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        int[] matriz = {7, 8, 5, 9, 6};
+        int indicePivot = acharIndicePivot(matriz);
+
+        System.out.println("O índice pivot é: " + indicePivot);
     }
-}
+    public static int acharIndicePivot(int[] nums) {
+        int somaTotal = 0;
+        int somaEsquerda = 0;
+
+        for (int num : nums) {
+            somaTotal += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (somaEsquerda == somaTotal - somaEsquerda - nums[i]) {
+                return i;
+            }
+            somaEsquerda += nums[i];
+        }
+
+        return -1;
+            }
+        }
